55795b77e8fb2552a99f9688b9cac01a3ce9ee42
matychp <matychp@gmail.com>
2014-09-28 21:56:20 -0300

    Actualizada DoubleList con iterador con doble sentido

--- DoubleList/src/doublelist/Principal.java
+++ DoubleList/src/doublelist/Principal.java
@@ -18,21 +18,25 @@ public class Principal {
         DoubleList<Integer> dl = new DoubleList<>();
         
         //Insertando elementos al comienzo y al final. Funciona
-        dl.addFirst(1);
+        dl.addFirst(3);
         dl.addFirst(2);
-        dl.addLast(3);
+        dl.addFirst(1);
+        dl.addFirst(0);
         dl.addLast(4);
-        dl.addFirst(5);
-        dl.addFirst(6);
+        dl.addLast(5);
+        dl.addLast(6);
         dl.addLast(7);
-        dl.addLast(8);
         
-        //Devolviendo pero sin eliminar elementos en dicha posicion en la lista.
+        //Remueve y Muestra el primero o el ultimo de la lista. Funciona
+        System.out.println(dl.removeFirst());
+        System.out.println(dl.removeLast());
+        
+        //Devolviendo pero sin eliminar elementos en dicha posicion en la lista. Funciona
         System.out.println(dl.get(2));
         System.out.println(dl.get(5));
         
         //Primero se muestra por pantalla el valor sobreplantado, y luego con el get() 
-        //se muestra el nuevo valor que se insertó anteriormente como segundo parametro en el set.
+        //se muestra el nuevo valor que se insertó anteriormente como segundo parametro en el set. Funciona
         System.out.println(dl.set(2, 10));
         System.out.println(dl.get(2));
         
